e7a3a9a1940373f15cacbef79e98646b758e2d38
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-02-28 19:17:51 +0700

    Iam change syntax in repo Solidariity

---
+++ Solidarity/application/controllers/Fhrzl.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Fhrzl extends CI_Controller {
+
+    public function index(){
+        echo "Ini adalah controler fhrzl";
+    }
+
+    public function profil(){
+        echo "Ini function profil";
+    }
+}

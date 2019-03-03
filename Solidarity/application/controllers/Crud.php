a4457df0b391c64dc91dd09d7301b9d87367616a
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-03-03 23:25:50 +0700

    Belajar

---
+++ Solidarity/application/controllers/Crud.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Crud extends CI_Controller {
+    public function_construct(){
+
+    }
+    public function index($offset=0){
+
+    }
+    public function add(){
+
+    }
+    public function delete($NIM = null){
+
+    }
+}

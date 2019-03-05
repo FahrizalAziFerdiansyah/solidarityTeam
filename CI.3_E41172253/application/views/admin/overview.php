e29c3fe50c990740214b0723efc8cc6be1add6f1
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-03-05 10:28:13 +0700

    Update overview.php

--- CI.3_E41172253/application/views/admin/overview.php
+++ CI.3_E41172253/application/views/admin/overview.php
@@ -15,7 +15,7 @@
   <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
 
   <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url('assets/fontawesome-free/css/fontawesome.min.css') ?>" rel="stylesheet" type="text/css">
 
   <!-- Page level plugin CSS-->
   <link href="<?php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">

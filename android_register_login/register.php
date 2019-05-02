ecdba2404bba1a00623846637ff50ef4b46c2d77
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-05-03 01:33:02 +0700

    ahha

---
+++ android_register_login/register.php
@@ -0,0 +1,29 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    require_once 'connect.php';
+
+    $sql = "INSERT INTO bengkel_keliling (username, password, email) VALUES ('$username', '$password', '$email')";
+
+    if (mysqli_query($conn, $sql) ) {
+        $result["success"] = "1";
+        $result["message"] = "success";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    } else {
+        $result["success"] = "0";
+        $result["message"] = "error";
+
+        echo json_encode($result);
+        mysqli_close($conn);
+    }
+}
+?>

406a4a164816dba69e57602abca52d9f6505bc69
niske04 <a1niske.elmy19@gmail.com>
2019-07-10 21:29:58 +0700

    about fix

---
+++ ProjectKita/about-us.php
@@ -0,0 +1,306 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="img/favicon.png" type="image/png">
+        <title>Beling Construction Multi</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body>
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+           	<div class="top_menu row m0">
+           		<div class="container">
+					<div class="float-left">
+						<ul class="list header_social">
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a href="#"><i class="fa fa-behance"></i></a></li>
+						</ul>
+					</div>
+					<div class="float-right">
+						<select class="lan_pack">
+							<option value="1">Indonesia</option>
+							<option value="1">English</option>
+							<option value="1">Aus</option>
+						</select>
+						<a class="dn_btn" href="#">Free Quote!</a>
+					</div>
+           		</div>	
+           	</div>	
+            <div class="main_menu">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+						<a href="#"><h3> <font color=orange> BELING </font></h3></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item"><a class="nav-link" href="index.php">Beranda</a></li> 
+								<li class="nav-item active"><a class="nav-link" href="about-us.php">Tentang</a></li> 
+								<li class="nav-item"><a class="nav-link" href="service.php">Service</a>
+									<li class="nav-item"><a class="nav-link" href="sparepart.php">Sparepart</a>
+								</li> 
+
+								
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li class="nav-item"><a href="#" class="search"><i class="lnr lnr-magnifier"></i></a></li>
+							</ul>
+						</div> 
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <section class="banner_area">
+            <div class="banner_inner d-flex align-items-center">
+            	<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+				<div class="container">
+					<div class="banner_content text-center">
+					<h2> <span> Tentang Kami </span></h2>
+					<a><font color=white>Sekilas tentang beling</font></a>
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+        
+        <!--================About Area =================-->
+        <section class="about_area p_120">
+        	<div class="container">
+        		<div class="row about_inner">
+        			<div class="col-lg-6">
+        				<div class="ab_left_text">
+						<h3 class="mb-30 title_color">Servis motor tak perlu pergi ke Bengkel</h3>
+        					<p><strong>BELING</strong> merupakan sebuah website bengkel motor online yang memberikan solusi dan kemudahan bagi
+								setiap permasalahan perawatan sepeda motor anda. dengan sumber daya tenaga 
+								montir yang dibekali keahlian dan profesionalisme di bidangnya, kami datangkan 
+								langsung ke tempat tujuan sesuai dengan pemesanan anda. Dengan adanya sistem yang
+								relatif mudah dilakukan kapanpun dan dimanapun hanya dengan bermodalkan koneksi internet
+								dan smart phone. Kami juga memberikan kenyamanan dan keramahan bagi setiap pelayanan yang kami
+								berikan demi terjaganya kepuasan dan kepercayaan pelanggan.
+
+							</div>
+        			</div>
+        			<div class="col-lg-6">
+        				<div class="ab_img">
+        					<img class="img-fluid" src="img/about-1.jpg" alt="">
+        				</div>
+        	</div>
+        </section>
+        <section class="team_area p_120">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Montir Profesional</h2>
+        			<p>Bengkel Keliling <strong>(BELING)</strong></p>
+        		</div>
+        		<div class="row team_inner">
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="img-fluid" src="img/team/team-1.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Faisal Abidin</h4>
+        						<p>Cepat, Tepat, dan Terpercaya</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="img-fluid" src="img/team/team-2.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>M. David</h4>
+        						<p>Profesional dan Tangkas</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="img-fluid" src="img/team/team-3.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ahmad Fattah</h4>
+        						<p>Cepat, Ahli dibidangnya</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="img-fluid" src="img/team/team-4.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Rendhy Putra</h4>
+        						<p>Profesional dan Tepat Waktu</p>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Team Area =================-->
+        
+        <!--================Testimonials Area =================-->
+        <section class="testimonials_area p_120">
+		<div class="banner_content text-center">
+        			<h2>Apa Kata Mereka ?</h2>
+        	<div class="container">
+        		<div class="testi_slider owl-carousel">
+        			<div class="item">
+        				<div class="testi_item">
+							<div class="media">
+								<div class="d-flex">
+									<img src="img/testimonials/testi-1.png" alt="">
+								</div>
+								<div class="media-body">
+									<p>Pelayanan <strong>BELING</strong> sangat memuaskan, dan juga memberikan kemudahan dalam melakukan servis. tak perlu
+									pergi ke bengkel, montir bisa datang ke rumah.</p>
+									<h4>Ahmad Laili</h4>
+									<h5>Tukang Ojek Online</h5>
+								</div>
+							</div>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="testi_item">
+							<div class="media">
+								<div class="d-flex">
+									<img src="img/testimonials/testi-2.png" alt="">
+								</div>
+								<div class="media-body">
+									<p>Tak perlu repot dan tak perlu antri, apalagi saya seorang pelajar, yang biasanya tidak pernah
+									ada waktu luang untuk memperbaiki kendaraan <strong>BELING</strong> memberikan kemudahan. </p>
+									<h4>Mahendra Setiyawan</h4>
+									<h5>Mahasiswa</h5>
+								</div>
+							</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+        <!--================End Clients Logo Area =================-->
+        
+        <!--================ start footer Area  =================-->	
+		<footer class="footer-area p_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3  col-md-6 col-sm-6">
+                        <div class="single-footer-widget ab_wd">
+                        <h6 class="footer_title">BELING</h6>
+            <p>Sistem informasi yang menyediakan perbaikan secara online.</p>
+          </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
+                        <div class="single-footer-widget contact_wd">
+                            <h6 class="footer_title">Kontak</h6>
+                            <p>
+                            Jl.Bondowoso<br>
+                            Bondowoso, B 535022<br>
+              Indenesia <br>
+              <strong>Phone:</strong> 085736586636<br>
+              <strong>Email:</strong> beling@gmail.com<br>			
+			  </p>	
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+              </div>
+      </div>
+    </div>
+    <div class="col-lg-4  col-md-6 col-sm-6">
+              <h6 class="footer_title">PELAYANAN</h6>
+            <p>Menyediakan pelayanan secara lengkap mulai ganti sparepart, konsultasi, dan ganti sparepart.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit"  value="Kirim">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+                    </div>
+                </div>
+				<div class="row footer-bottom d-flex justify-content-between align-items-center">
+                
+                <div class="section-top-border text-center">
+                </div>
+                <div class="copyright">
+        &copy; Copyright <strong><font color=white>BELING</font></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+        </footer>
+		<!--================ End footer Area  =================-->
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="js/theme.js"></script>
+    </body>
+</html>

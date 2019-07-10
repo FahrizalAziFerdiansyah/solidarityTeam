796abd0fccc4c2c6f825726a79891443e38c88d6
SyahdanFimanHuda <48002079+SyahdanFimanHuda@users.noreply.github.com>
2019-07-10 18:33:05 +0700

    servis 2

--- ProjectKita/application/views/user/servis.php
+++ ProjectKita/application/views/user/servis.php
@@ -31,3 +31,43 @@
 							<li><a href="#"><i class="fa fa-behance"></i></a></li>
 						</ul>
 					</div>
+                    <div class="float-right">
+						<select class="lan_pack">
+							<option value="1">Indonesia</option>
+							<option value="1">English</option>
+							<option value="1">Aus</option>
+						</select>
+						<a class="dn_btn" href="#">Free Quote!</a>
+					</div>
+           		</div>	
+           	</div>	
+            <div class="main_menu">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+						<a href="#"><h3> <font color=orange> BELING </font></h3></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav ml-auto">
+								<li class="nav-item"><a class="nav-link" href="index.php">Beranda</a></li> 
+								<li class="nav-item"><a class="nav-link" href="about-us.php">Tentang</a></li> 
+								<li class="nav-item active"><a class="nav-link" href="service.php">Service</a>
+									<li class="nav-item"><a class="nav-link" href="sparepart.php">Sparepart</a>
+								</li> 
+
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li class="nav-item"><a href="#" class="search"><i class="lnr lnr-magnifier"></i></a></li>
+							</ul>
+						</div> 
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
